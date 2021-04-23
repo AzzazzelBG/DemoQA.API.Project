4787a03cffbc3a797e68f75c0655a3c84c4709b0
Alexander Drinkov <adrinkov@gmail.com>
2021-04-23 11:39:29 +0300

    Create RestClient class

---
+++ src/DemoQA.HttpClients/HttpClients/UnAuthenticatedRestClient.cs
@@ -0,0 +1,20 @@
+﻿using RestSharp;
+using System;
+
+namespace DemoQA.HttpClients
+{
+    public class UnAuthenticatedRestClient
+    {
+        private IRestClient _client;
+
+        public UnAuthenticatedRestClient(string url)
+        {
+            _client = new RestClient(url)
+            {
+                UserAgent = "Demo QA Test Agent",
+            };
+        }
+
+        public IRestClient Client => _client;
+    }
+}
