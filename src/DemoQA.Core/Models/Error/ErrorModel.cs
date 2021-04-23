b5fdc8bba0ab14c8063915eb78de31f6bcdc970e
Alexander Drinkov <adrinkov@gmail.com>
2021-04-23 11:52:24 +0300

    Create Error model

---
+++ src/DemoQA.Core/Models/Error/ErrorModel.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace DemoQA.Core.Models.Error
+{
+    public class ErrorModel
+    {
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+    }
+}
